f335183375d49bf8c08599ad91aa7de9ca3898a7
pabkramar <pabkramar@alum.us.es>
2023-05-30 14:30:59 +0200

    pipeline

--- app/src/main/java/git/repos/FirstDayAtSchool.java
+++ app/src/main/java/git/repos/FirstDayAtSchool.java
@@ -1,10 +1,10 @@
 /**
- * Clase a probar en el código ejemplo para el control de junio de 2022
+ * Clase a probar en el cï¿½digo ejemplo para el control de junio de 2022
  */
-package muit.fs.junio22;
+package git.repos;
 
 /**
- * @author Isabel Román
+ * @author Isabel Romï¿½n
  *
  */
 
@@ -34,7 +34,7 @@ public class FirstDayAtSchool {
 	}
 
 	public int getNumberOf(String thingName) {
-		int number = 10;
+		int number = 0;
 		if (schoolbag.containsKey(thingName)) {
 			number = number + schoolbag.get(thingName).intValue();
 		}
--- app/src/test/java/git/repos/FirstDayAtSchoolTest.java
+++ app/src/test/java/git/repos/FirstDayAtSchoolTest.java
@@ -1,4 +1,4 @@
-package muit.fs.junio22;
+package git.repos;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
